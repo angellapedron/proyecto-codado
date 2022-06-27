a3b340ca06dd2db279bc078a7fc3479295d1c1f9
angellapedron <castanedaangella@gmail.com>
2022-06-27 19:43:02 -0400

    Formulario_Requisitos

---
+++ components2.0/pages/Formulario.js
@@ -0,0 +1,83 @@
+import React , {useState, useEffect} from 'react'
+import {useForm} from 'react-hook-form';
+import Navbar from '../Navbar';
+import '../Formulario.css';
+
+const Formulario = () => {
+  const {register, handleSubmit, formState:{ errors }} = useForm()
+
+  const customSubmit = (data) => {
+    console.log(data)
+
+  }
+
+  const [cursos, setCursos] = useState([])
+
+  useEffect(() =>{
+    const data = require('../CursosData.json')
+    console.log(data)
+    setCursos(data)
+  },[])
+
+  return (
+
+    <>
+    <Navbar />
+    <div className="background">
+    <div className="container">
+    <h2>Pre-Inscripcion</h2>
+    <form onSubmit ={handleSubmit(customSubmit)} className="form-react">
+      <div className='form-control'>
+        <label>Nombre</label>
+        <input className ="controls" type="text" {...register('name', {required:true, maxLength:100})}/>
+        {errors.name?.type === 'required' && <small className='fail'>El campo no puede estar vacio</small> }
+        {errors.name?.tupe === 'maxLength' && <small className='fail'>Se excedio del maximo de caracteres</small> }
+      </div>
+      <div className='form-control'>
+        <label>Cedula de Identidad</label>
+        <input className="controls" type="text" {...register('cedula', {required:true})}/>
+        {errors.cedula?.type === 'required' && <small className='fail'>El campo no puede estar vacio</small> }
+      </div>
+      <div className='form-control'>
+        <label>Edad</label>
+        <input className="controls" type="number" {...register('edad', {required:true, min:18, max:65})}/>
+        {errors.edad?.type === 'required' && <small className='fail'>El campo no puede estar vacio</small> }
+        {errors.edad?.type === 'min' && <small className='fail'>La edad minima es de 18 años</small> }
+        {errors.edad?.type === 'max' && <small className='fail'>La edad maxima es de 65 años</small> }
+      </div>
+      <div className='form-control'>
+        <label>Telefono</label>
+        <input className="controls" type="number" {...register('number', {required:true})}/>
+        {errors.number?.type === 'required' && <small className='fail'>El campo no puede estar vacio</small> }
+      </div>
+      <div className='form-control'>
+        <label>Curso</label>
+        <select className='controls' {...register('cursos', {required:true})}>
+          <option value=''>--Elegir Curso--</option>
+          {cursos.map((item) =>{
+            return(
+              <option key={item.cursos}>
+                {item.cursos}
+              </option>
+            )
+          })}
+        </select>
+        {errors.cursos?.type === 'required' && <small className='fail'>El campo no puede estar vacio</small> }
+      </div>
+      <div className='form-control'>
+        <label>Correo</label>
+        <input className="controls" type="text" {...register('correo', {required:true, pattern: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i})}/>
+        {errors.correo?.type === 'required' && <small className='fail'>El campo no puede estar vacio</small> }
+        {errors.correo?.type === 'pattern' && <small className='fail'>El formato del correo es incorrecto</small> }
+      </div>
+      <button type='submit'>Enviar</button>
+      <p>Toda informacion marcada al momento de ser entregada se tendra en consideracion como informacion confidencial, dicha informacion esta protegida por los derechos de propiedad industrial y programas informaticos. 
+        @2022 Instituto Codado. Todos los derechos reservados</p>
+    </form>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Formulario
---
+++ components2.0/pages/Requerimientos.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import '../../App.css';
+import Navbar from '../Navbar';
+import Card from '../Card';
+
+
+
+
+function Requerimientos() {
+  return (
+    <>
+    <Navbar />
+    <div className="body">
+    <div className='wrapper'>
+      <Card
+      img = "https://source.unsplash.com/64YrPKiguAE"
+      title = "Asistente de Informatica"
+      descripcion = " Los agentes de soporte técnico informático brindan asistencia o soporte a la empresa o a los empleados acerca de cuestiones relacionadas con el mantenimiento de hardware, redes y sistemas. Pueden hacerlo personalmente o de manera remota, por teléfono o a través de correo electrónico."
+      text = " Requisitos:"
+      text1=" • 	Cursado bachillerato y con inclinaciones a estudios superiores en Ciencias de la Informatica"
+      text2=" •  Ser mayor de 18 años"
+      text3=" • 	Contar con Rif"
+      text4=" •  Fotocopia de la Cedula de Identidad"
+      modulo=" Programa del curso"
+      modulo1=" • Modulo I: "
+      modulo2=" • Modulo II:"
+      modulo3=" • Modulo III:"
+      modulo4=" • Modulo IV:"
+      modulo5=" • Modulo V: "
+      info=" Duracion"
+      info1="12 meses. (1.190 horas académicas teóricas-prácticas)"
+      info_h=" Horario"
+      info2="Horario de lunes a viernes de 2pm a 5pm  
+      (para grupos de mínimo 15 personas)"
+      />
+
+      <Card
+      img = "https://source.unsplash.com/UK1N66KUkMk"
+      title = "Asistente Administrativo"
+      descripcion="El Asistente Administraivo, está capacitado para desenvolverse tanto en las Actividades Administrativas y Contables. Desempeñando Habilidades y Destrezas en las distintas Actividades de una Empresa. Realiza Procesos en forma manual u operando Equipos y Programas Computarizados Actualizados. Capacidad de Trabajo en Equipo y Bajo Presión."
+      text=" Requistos:"
+      text1=" • Cursado bachillerato y con inclinaciones a estudios superiores en Adminitracion"
+      text2=" • Ser mayor de 18 años"
+      text3=" • Contar con Rif"
+      text4=" • Fotocopia de la Cedula de Identidad"
+      modulo=" Programa del curso"
+      modulo1=" • Modulo I: Archivo"
+      modulo2=" • Modulo II: Relaciones Publicas"
+      modulo3=" • Modulo III: Nomina"
+      modulo4=" • Modulo IV: Contabilidad"
+      modulo5=" • Modulo V: Computacion"
+      info=" Duracion"
+      info1="6 meses. (192 horas académicas teóricas-prácticas)"
+      info_h=" Horario"
+      info2="Martes y Jueves de 3:00pm a 6:00pm
+      (para grupos de mínimo 15 personas)"
+      />
+
+      <Card
+      img = "https://source.unsplash.com/BlKBaiFdNgA"
+      title = "Asistente Financiero"
+      descripcion = "Entre las responsabilidades de un asistente financiero se incluyen el procesamiento de pagos, el mantenimiento de informes de costos y la gestión de facturas. Si tienes experiencia en puestos de contabilidad y estás interesado en procedimientos como la gestión de impuestos y la preparación de presupuestos, nos gustaría conocerte."
+      text="  Requisitos:"
+      text1=" • Cursado bachillerato y con inclinaciones a estudios superiores en Finanzas"
+      text2=" • Ser mayor de 18 años"
+      text3=" • Contar con Rif"
+      text4=" • Fotocopia de la Cedula de Identidad"
+      modulo=" Programa del curso"
+      modulo1=" • Modulo I: "
+      modulo2=" • Modulo II:"
+      modulo3=" • Modulo III:"
+      modulo4=" • Modulo IV:"
+      modulo5=" • Modulo V: "
+      info=" Duracion"
+      info1="6 meses. (192 horas académicas teóricas-prácticas)"
+      info_h=" Horario"
+      info2="Martes y Jueves de 2:00pm a 4:00pm
+      (para grupos de mínimo 15 personas)"
+      />
+
+
+      <Card
+      img = "https://source.unsplash.com/5fNmWej4tAA"
+      title = "Asistente Contable"
+      descripcion = "La persona que opte por adquirir destrezas como Asistente Contable podrá aportar a la ejecución de las transacciones generales de la empresa. También calculará y elaborará roles de pago y manejará de forma adecuada las herramientas informáticas de los diferentes organismos de control como el Servicio de Rentas Internas, Ministerio de Trabajo, Instituto Ecuatoriano de Seguridad Social, entre otros."
+      text=" Requisitos:"
+      text1=" • Cursado bachillerato y con inclinaciones a estudios superiores en Contabilidad"
+      text2=" • Ser mayor de 18 años"
+      text3=" • Contar con Rif"
+      text4=" • Fotocopia de la Cedula de Identidad"
+      modulo=" Programa del curso"
+      modulo1=" • Modulo I: "
+      modulo2=" • Modulo II:"
+      modulo3=" • Modulo III:"
+      modulo4=" • Modulo IV:"
+      modulo5=" • Modulo V: "
+      info=" Duracion"
+      info1="6 meses. (192 horas académicas teóricas-prácticas)"
+      info_h=" Horario"
+      info2="Lunes y Miercoles de 3:00pm a 6:00pm
+      (para grupos de mínimo 15 personas)"
+      />
+
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Requerimientos
